78a4bba5662ff6e87eaf7b6b3d589a5118194ecb
ANAHI PILLAJO <168648107+anahipillajot@users.noreply.github.com>
2024-11-08 14:11:20 -0500

    mas ejercicios

---
+++ src/Poliretos_G4/Loading/Loading_01.java
@@ -0,0 +1,25 @@
+package Poliretos_G4.Loading;
+
+public class Loading_01 {
+    public void G4_Loading_1() {
+        System.out.println("Loading 1");
+        String[] rotacion = {"|", "/", "-", "\\"};
+        int carga = 0;
+
+        while (carga <= 100) {
+            System.out.print("\r" + rotacion[carga % 4] + " " + carga + "%");
+            carga += 5;
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                System.out.println("\nLa operación fue interrumpida.");
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+
+        System.out.println("\nCarga completa: 100%");
+    }
+
+}
